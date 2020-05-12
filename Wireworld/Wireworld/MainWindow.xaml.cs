a966e8426a20184ae19d643724f3b9b48405f75e
KonradMagiera <k.magiera1@wp.pl>
2020-05-12 11:57:21 +0200

    wireworld settings in GUI; drawing improvement

--- Wireworld/Wireworld/MainWindow.xaml.cs
+++ Wireworld/Wireworld/MainWindow.xaml.cs
@@ -15,64 +15,116 @@ namespace Wireworld
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int NodeNumber { get; set; } = 20;
-        private double NodeSize { get; set; }
-        private NodeType[,] tmp;
+        private readonly int BOARD_MARGIN = 8;
 
         public MainWindow()
         {
             InitializeComponent();
-            NodeSize = canvasBoard.Height / NodeNumber;
+            NodeSize = (canvasBoard.Height - BOARD_MARGIN) / NodeNumber;
             tmp = new NodeType[NodeNumber, NodeNumber];
 
-            // Test
-            tmp[5, 4] = NodeType.Head;
-            tmp[3, 9] = NodeType.Conductor;
-            tmp[9, 0] = NodeType.Tail;
             Draw();
 
         }
 
+
+        private int NodeNumber { get; set; } = 20;
+        private double NodeSize { get; set; }
+
+        // Variables below probably should be moved to backend
+        private NodeType[,] tmp;
+        private int NumberOfGenerations { get; set; }
+        private int NumberOfThreads { get; set; }
+
+
+        /// <summary>
+        /// Draw whole board.
+        /// </summary>
         private void Draw()
         {
+            canvasBoard.Children.Clear();
+
             for (int i = 0; i < NodeNumber; i++)
                 for (int j = 0; j < NodeNumber; j++)
                 {
-                    Rectangle r = new Rectangle
-                    {
-                        Height = NodeSize,
-                        Width = NodeSize,
-                        Stroke = Brushes.Gray,
-                        StrokeThickness = 0.3
-                    };
-
-                    if (tmp[i, j] == NodeType.Empty) r.Fill = Brushes.Black;
-                    else if (tmp[i, j] == NodeType.Conductor) r.Fill = Brushes.Yellow;
-                    else if (tmp[i, j] == NodeType.Head) r.Fill = Brushes.Blue;
-                    else if (tmp[i, j] == NodeType.Tail) r.Fill = Brushes.Red;
-
-                    Canvas.SetLeft(r, NodeSize * i);
-                    Canvas.SetTop(r, NodeSize * j);
-                    canvasBoard.Children.Add(r);
+                    Draw(i, j);
                 }
+        }
+
+        /// <summary>
+        /// Draw single Rectangle.
+        /// </summary>
+        /// <param name="x">Position X of Rectangle</param>
+        /// <param name="y">Position Y of Rectangle</param>
+        private void Draw(int x, int y)
+        {
+            Rectangle r = new Rectangle
+            {
+                Height = NodeSize,
+                Width = NodeSize,
+                Stroke = Brushes.Gray,
+                StrokeThickness = 0.3
+            };
+
+            if (tmp[x, y] == NodeType.Empty) r.Fill = Brushes.Black;
+            else if (tmp[x, y] == NodeType.Conductor) r.Fill = Brushes.Yellow;
+            else if (tmp[x, y] == NodeType.Head) r.Fill = Brushes.Blue;
+            else if (tmp[x, y] == NodeType.Tail) r.Fill = Brushes.Red;
+
+            Canvas.SetLeft(r, NodeSize * x);
+            Canvas.SetTop(r, NodeSize * y);
+            canvasBoard.Children.Add(r);
         }
 
         private void CanvasUserClick(object sender, MouseButtonEventArgs e)
         {
-            Console.WriteLine("Test");
             Point Node = Mouse.GetPosition(canvasBoard);
             int x = (int)(Node.X / NodeSize);
             int y = (int)(Node.Y / NodeSize);
 
-            if(x >= 0 && y >= 0 && x < NodeNumber && y < NodeNumber)
+            if (x >= 0 && y >= 0 && x < NodeNumber && y < NodeNumber)
             {
-                if (tmp[x, y] == NodeType.Empty) tmp[x,y] = NodeType.Conductor;
+                if (tmp[x, y] == NodeType.Empty) tmp[x, y] = NodeType.Conductor;
                 else if (tmp[x, y] == NodeType.Conductor) tmp[x, y] = NodeType.Head;
                 else if (tmp[x, y] == NodeType.Head) tmp[x, y] = NodeType.Tail;
                 else if (tmp[x, y] == NodeType.Tail) tmp[x, y] = NodeType.Empty;
-                Draw();
+                Draw(x, y);
+            }
+        }
+
+        private void RunSimulation(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                NumberOfGenerations = int.Parse(numberOfGenerations.Text);
+                NumberOfThreads = int.Parse(numberOfThreads.Text);
+            }
+            catch
+            {
+                Console.WriteLine("Given settings are incorrect! Only numbers are accepted.");
             }
 
+            Console.WriteLine($"NumberOfGenerations: {NumberOfGenerations}, NumberOfThreads {NumberOfThreads}");
+        }
+
+        private void ChangeBoardSize(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int tmpSize = int.Parse(sizeOfBoard.Text);
+
+                if (tmpSize != NodeNumber)
+                {
+                    NodeNumber = tmpSize;
+                    NodeSize = (canvasBoard.Height - BOARD_MARGIN) / NodeNumber;
+                    tmp = new NodeType[NodeNumber, NodeNumber];
+                    Draw();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Given size is incorrect! Only numbers are accepted.");
+            }
         }
     }
 }
